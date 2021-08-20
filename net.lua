ba36bc0f0e7ab9d2511a7e2a9486881eccfb238f
TypicallyAUser <85001520+TypicallyAUser@users.noreply.github.com>
2021-08-19 20:07:45 -0400

    Update net.lua

--- net.lua
+++ net.lua
@@ -1,5 +1,8 @@
 if _G.typicalcool ~= true then
     _G.typicalcool = true
+    if setclipboard then
+        setclipboard("net made for roware | join our discord server : https://discord.gg/9UaMPPb9N9")
+    end
     game:GetService("RunService").Heartbeat:Connect(function()
        workspace.FallenPartsDestroyHeight = 0/0
        settings().Physics.AllowSleep = false
