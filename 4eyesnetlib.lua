c24681047028ffadb8f07e50a7c6a08c4bdf00f9
typical <85001520+TypicallyAUser@users.noreply.github.com>
2023-01-07 20:39:49 -0500

    Update 4eyesnetlib.lua

--- 4eyesnetlib.lua
+++ 4eyesnetlib.lua
@@ -1,127 +1,77 @@
 
-local Players = game:GetService("Players")
-local RunService = game:GetService("RunService")
-local LocalPlayer = Players.LocalPlayer
---[[
-Network Library by 4eyes
-This script is meant for debugging and game testing ONLY. Any other use is not encouraged.
+local Player=game.Players.LocalPlayer
+local RunService=game:GetService("RunService")
 
-The basic concepts of Network Ownership for anyone interested:
-1. Parts not network owned by server or another player will be owned by the player closest to it.
-2. To retain network ownership, you must be constantly sending physics packets or people may be able to take ownership, as your network is contested when you aren't sending physics packets.
+-- Start the chaos!
 
-Usage: Put this in your script, run the PartOwnership enable coroutine [ coroutine.resume(Network["PartOwnership"]["Enable"]) ] and use Network.RetainPart(Part) on any part you'd like to retain ownership over, then just apply a replicating method of movement. Network["RemovePart"](Part) to remove ownership of a part, and run the PartOwnership Disable coroutine [ coroutine.resume(Network["PartOwnership"]["Disable"]) ] to stop. Set Network.CharacterRelative to false if you change your character to a non-replicating client model. Credit me if you'd like.
-
-Example script:
-loadstring(game:HttpGet("https://raw.githubusercontent.com/your4eyes/RobloxScripts/main/Net_Library.lua"))()
-coroutine.resume(Network["PartOwnership"]["Enable"])
---Network.CharacterRelative = false --for reanimates
-Network.RetainPart(Part)
-
---]]
 if not getgenv().Network then
+	warn([[
 
+	username's net library
+
+	based off of 4eye's net library released on v3rmillion.
+
+	reworked for project mint's reanimation
+
+	]])
+	
+	warn("[NETWORK]: Loading...")
+	
 	getgenv().Network = {
-		BaseParts = {};
-		FakeConnections = {};
-		Connections = {};
-		Output = {
-			Enabled = true;
-			Prefix = "[NETWORK] ";
-			Send = function(Type,Output,BypassOutput)
-				if typeof(Type) == "function" and (Type == print or Type == warn or Type == error) and typeof(Output) == "string" and (typeof(BypassOutput) == "nil" or typeof(BypassOutput) == "boolean") then
-					if Network["Output"].Enabled == true or BypassOutput == true then
-						Type(Network["Output"].Prefix..Output);
-					end;
-				elseif Network["Output"].Enabled == true then
-					error(Network["Output"].Prefix.."Output Send Error : Invalid syntax.");
-				end;
-			end;
-		};
-		CharacterRelative = false;
+		Enabled = false;
+		Instances = {};
 	}
-
-	Network["Output"].Send(print,": Loading.")
-	Network["Velocity"] = Vector3.new(14.46262424,14.46262424,14.46262424); --exactly 25.1 magnitude
-	Network["RetainPart"] = function(Part,ReturnFakePart) --function for retaining ownership of unanchored parts
-		assert(typeof(Part) == "Instance" and Part:IsA("BasePart") and Part:IsDescendantOf(workspace),Network["Output"].Prefix.."RetainPart Error : Invalid syntax: Arg1 (Part) must be a BasePart which is a descendant of workspace.")
-		assert(typeof(ReturnFakePart) == "boolean" or typeof(ReturnFakePart) == "nil",Network["Output"].Prefix.."RetainPart Error : Invalid syntax: Arg2 (ReturnFakePart) must be a boolean or nil.")
-		if not table.find(Network["BaseParts"],Part) then
-			if Network.CharacterRelative == true then
-				local Character = LocalPlayer.Character
-				if Character and Character.PrimaryPart then
-					local Distance = (Character.PrimaryPart.Position-Part.Position).Magnitude
-					if Distance > 1000 then
-						Network["Output"].Send(warn,"RetainPart Warning : PartOwnership not applied to BasePart "..Part:GetFullName()..", as it is more than "..gethiddenproperty(LocalPlayer,"MaximumSimulationRadius").." studs away.")
-						return false
-					end
-				else
-					Network["Output"].Send(warn,"RetainPart Warning : PartOwnership not applied to BasePart "..Part:GetFullName()..", as the LocalPlayer Character's PrimaryPart does not exist.")
-					return false
-				end
-			end
-			table.insert(Network["BaseParts"],Part)
+	
+	Network.RetainPart = function(Part) -- Retain part just configures ONE property. I just dumbed it down.
+		assert(typeof(Part) and (Part:IsA("BasePart") or Part:IsA("MeshPart")),warn("[NETWORK]: Retain error: Unable to proceed due to Arg1 isn't a BasePart/MeshPart. Are you sure you put in the right instance?"))
+		if not table.find(Network["Instances"],Part) then
 			Part.CustomPhysicalProperties = PhysicalProperties.new(0,0,0,0,0)
-			Network["Output"].Send(print,"PartOwnership Output : PartOwnership applied to BasePart "..Part:GetFullName()..".")
-			if ReturnFakePart == true then
-				return FakePart
-			end
+			table.insert(Network["Instances"],Part)
 		else
-			Network["Output"].Send(warn,"RetainPart Warning : PartOwnership not applied to BasePart "..Part:GetFullName()..", as it already active.")
-			return false
+			warn("[NETWORK]: Instance '"..Part.Name.."' is already being retained!")
 		end
 	end
-
-	Network["RemovePart"] = function(Part) --function for removing ownership of unanchored part
-		assert(typeof(Part) == "Instance" and Part:IsA("BasePart"),Network["Output"].Prefix.."RemovePart Error : Invalid syntax: Arg1 (Part) must be a BasePart.")
-		local Index = table.find(Network["BaseParts"],Part)
-		if Index then
-			table.remove(Network["BaseParts"],Index)
-			Network["Output"].Send(print,"RemovePart Output: PartOwnership removed from BasePart "..Part:GetFullName()..".")
-		else
-			Network["Output"].Send(warn,"RemovePart Warning : BasePart "..Part:GetFullName().." not found in BaseParts table.")
-		end
-	end
-
-	Network["SuperStepper"] = Instance.new("BindableEvent") --make super fast event to connect to
-	for _,Event in pairs({RunService.Stepped,RunService.Heartbeat}) do
+	
+	-- 4Eyes made this. what a really powerful thing! edited a small bit.
+	Network["SuperStepper"] = Instance.new("BindableEvent")
+	for i,Event in pairs({RunService.Stepped,RunService.Heartbeat,RunService.RenderStepped}) do
 		Event:Connect(function()
 			return Network["SuperStepper"]:Fire(Network["SuperStepper"],tick())
 		end)
 	end
-
+	
+	-- EDITED A TAD BIT TO WORK FLAWLESSLY
 	Network["PartOwnership"] = {};
 	Network["PartOwnership"]["PreMethodSettings"] = {};
 	Network["PartOwnership"]["Enabled"] = false;
 	Network["PartOwnership"]["Enable"] = coroutine.create(function() --creating a thread for network stuff
 		if Network["PartOwnership"]["Enabled"] == false then
 			Network["PartOwnership"]["Enabled"] = true --do cool network stuff before doing more cool network stuff
-			Network["PartOwnership"]["PreMethodSettings"].ReplicationFocus = LocalPlayer.ReplicationFocus
-			LocalPlayer.ReplicationFocus = workspace
-			Network["PartOwnership"]["PreMethodSettings"].SimulationRadius = gethiddenproperty(LocalPlayer,"SimulationRadius")
+			Network["PartOwnership"]["PreMethodSettings"].ReplicationFocus = Player.ReplicationFocus
+			Player.ReplicationFocus = workspace
+			Network["PartOwnership"]["PreMethodSettings"].SimulationRadius = gethiddenproperty(Player,"SimulationRadius")
 			Network["PartOwnership"]["Connection"] = Network["SuperStepper"].Event:Connect(function() --super fast asynchronous loop
-				sethiddenproperty(LocalPlayer,"SimulationRadius",1/0)
+				sethiddenproperty(Player,"SimulationRadius",1/0)
 			end)
-			Network["Output"].Send(print,"PartOwnership Output : PartOwnership enabled.")
+			warn("[NETWORK] PartOwnership Output : PartOwnership enabled.")
 		else
-			Network["Output"].Send(warn,"PartOwnership Output : PartOwnership already enabled.")
+			warn("[NETWORK] PartOwnership Output : PartOwnership already enabled.")
 		end
 	end)
 	Network["PartOwnership"]["Disable"] = coroutine.create(function()
 		if Network["PartOwnership"]["Connection"] then
 			Network["PartOwnership"]["Connection"]:Disconnect()
-			LocalPlayer.ReplicationFocus = Network["PartOwnership"]["PreMethodSettings"].ReplicationFocus
-			sethiddenproperty(LocalPlayer,"SimulationRadius",Network["PartOwnership"]["PreMethodSettings"].SimulationRadius)
+			Player.ReplicationFocus = Network["PartOwnership"]["PreMethodSettings"].ReplicationFocus
+			sethiddenproperty(Player,"SimulationRadius",Network["PartOwnership"]["PreMethodSettings"].SimulationRadius)
 			Network["PartOwnership"]["PreMethodSettings"] = {}
-			for _,Part in pairs(Network["BaseParts"]) do
-				Network["RemovePart"](Part)
-			end
 			Network["PartOwnership"]["Enabled"] = false
-			Network["Output"].Send(print,"PartOwnership Output : PartOwnership disabled.")
+			warn("[NETWORK] PartOwnership Output : PartOwnership disabled.")
 		else
-			Network["Output"].Send(warn,"PartOwnership Output : PartOwnership already disabled.")
+			warn("[NETWORK] PartOwnership Output : PartOwnership already disabled.")
 		end
 	end)
-	Network["Output"].Send(print,": Loaded.")
+	
+	warn("[NETWORK]: Successfully loaded.")
+else
+	warn("[NETWORK]: Already loaded!")
 end
--- coroutine.resume(Network["PartOwnership"]["Disable"])
